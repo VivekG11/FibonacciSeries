22717ca1f3d80e08c060ef1504c7ea647be2897a
VivekG11 <vivekvk2903@gmail.com>
2021-07-02 12:25:10 +0530

    [Implementing a swicth case to perform Operations]

---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LogicalProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Problems ..........");
+            /*...........................................................
+             * choose an option to perform particular operation
+             * .................................................*/
+            Console.WriteLine("Enter Your Choice :");
+            int options = Convert.ToInt32(Console.ReadLine());
+            /*..............................................................
+             * Choose options using switch case
+             * ................................................*/
+            switch (options)
+            {
+               /* case 1:
+                    Console.WriteLine("Fibonacci Series ......");
+                    Fibonacci.FindFibonacciSeries();
+                    break;*/
+                default:
+                    break;
+            }
+        }
+    }
+}
